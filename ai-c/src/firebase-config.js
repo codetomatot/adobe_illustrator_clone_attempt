6609395fb8c020e295b13f6c31606ec5aa4d03ce
hueychopper <75332544+hueychopper@users.noreply.github.com>
2021-10-17 11:15:58 -0400

    Update firebase-config.js

--- ai-c/src/firebase-config.js
+++ ai-c/src/firebase-config.js
@@ -2,13 +2,7 @@ import React from 'react';
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCFzHgoDSr46JmMugJ-ZZAJ-bf-ytmtMB8",
-    authDomain: "adobe-904f5.firebaseapp.com",
-    projectId: "adobe-904f5",
-    storageBucket: "adobe-904f5.appspot.com",
-    messagingSenderId: "884549359691",
-    appId: "1:884549359691:web:36e612ab98c88f9e32d5ce",
-    measurementId: "G-0XNY9QK8KN"
+    "YOUR-FIREBASE-PROJECT-INFO-HERE"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
